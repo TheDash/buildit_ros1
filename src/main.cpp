d6d5a06aaa582094dd3a81917ee48af859f48227
dash <dash@dash-ThinkPad-T530.(none)>
2015-01-09 22:28:24 -0500

    Now is a rosnode

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <QtGui/QApplication>
 #include <buildit_ros/mainwindow.h>
+#include <ros/ros.h>
+
 
 int main(int argc, char *argv[])
 {
+
+    ros::init(argc, argv, "buildit_ros");
+    ros::NodeHandle nh;
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
