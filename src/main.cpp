e5df95c3c405c3ca09fa49b81d69bd6517c98e5c
dash <dash@dash-ThinkPad-T530.(none)>
2015-01-09 22:31:58 -0500

    Added rviz visualization manager

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,10 @@ int main(int argc, char *argv[])
     ros::init(argc, argv, "buildit_ros");
     ros::NodeHandle nh;
 
+    // TODO Embed Rviz frame. 
+
     QApplication a(argc, argv);
+    MyViz* myviz = new MyViz();
     MainWindow w;
     w.show();
     
