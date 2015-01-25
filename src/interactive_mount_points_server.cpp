0fa1946aaaae4b80667634834e2467da0a2a017e
dash <dash@dash-ThinkPad-T530.(none)>
2015-01-25 18:00:05 -0500

    Updated old file, transfered marker srever to new main

---
+++ src/interactive_mount_points_server.cpp
@@ -0,0 +1,143 @@
+#include <ros/ros.h>
+#include <interactive_markers/interactive_marker_server.h>
+
+static void alignMarker(const InteractiveMarkerFeedbackConstPtr&);
+static void processFeedback( const InteractiveMarkerFeedbackConstPtr&);
+void makeChessPieceMarker(tf::Vector3& );
+
+boost::shared_ptr<interactive_markers::InteractiveMarkerServer> server;
+interactive_markers::MenuHandler menu_handler;
+visualization_msgs::Marker makeBox(InteractiveMarker&);  
+
+Marker makeBox(InteractiveMarker &msg )
+{
+	Marker marker;
+	marker.type = Marker::CUBE;
+	marker.scale.x = msg.scale * 0.45;
+	marker.scale.y = msg.scale * 0.45;
+	marker.scale.z = msg.scale * 0.45;
+	marker.color.r = 0.5;
+	marker.color.g = 0.5;
+	marker.color.b = 0.5;
+	marker.color.a = 1.0;
+	return marker;
+}
+
+
+//Taken from RViz tutorials 
+void makeChessPieceMarker(tf::Vector3& position)
+{
+  // Get the position from the link name. 
+  InteractiveMarker int_marker;
+  int_marker.header.frame_id = "back_right_wheel";
+  tf::pointTFToMsg(position, int_marker.pose.position);
+  int_marker.scale = 1;
+
+  int_marker.name = "back_right_wheel";
+  int_marker.description = "back_right_wheel";
+
+  InteractiveMarkerControl control;
+
+  control.orientation.w = 1;
+  control.orientation.x = 0;
+  control.orientation.y = 1;
+  control.orientation.z = 0;
+  control.interaction_mode = InteractiveMarkerControl::MOVE_PLANE;
+  int_marker.controls.push_back(control);
+
+  // make a box which also moves in the plane
+  control.markers.push_back( makeBox(int_marker) );
+  control.always_visible = true;
+  int_marker.controls.push_back(control);
+
+  // we want to use our special callback function
+  server->insert(int_marker);
+  server->setCallback(int_marker.name, &processFeedback);
+
+  // set different callback for POSE_UPDATE feedback
+  server->setCallback(int_marker.name, &alignMarker,   visualization_msgs::InteractiveMarkerFeedback::POSE_UPDATE );
+
+}
+
+
+void processFeedback( const InteractiveMarkerFeedbackConstPtr &feedback )
+{
+  std::ostringstream s;
+  s << "Feedback from marker '" << feedback->marker_name << "' "
+      << " / control '" << feedback->control_name << "'";
+
+  std::ostringstream mouse_point_ss;
+  if( feedback->mouse_point_valid )
+  {
+    mouse_point_ss << " at " << feedback->mouse_point.x
+                   << ", " << feedback->mouse_point.y
+                   << ", " << feedback->mouse_point.z
+                   << " in frame " << feedback->header.frame_id;
+  }
+
+  switch ( feedback->event_type )
+  {
+    case visualization_msgs::InteractiveMarkerFeedback::BUTTON_CLICK:
+      ROS_INFO_STREAM( s.str() << ": button click" << mouse_point_ss.str() << "." );
+      break;
+
+    case visualization_msgs::InteractiveMarkerFeedback::MENU_SELECT:
+      ROS_INFO_STREAM( s.str() << ": menu item " << feedback->menu_entry_id << " clicked" << mouse_point_ss.str() << "." );
+      break;
+
+    case visualization_msgs::InteractiveMarkerFeedback::POSE_UPDATE:
+      ROS_INFO_STREAM( s.str() << ": pose changed"
+          << "\nposition = "
+          << feedback->pose.position.x
+          << ", " << feedback->pose.position.y
+          << ", " << feedback->pose.position.z
+          << "\norientation = "
+          << feedback->pose.orientation.w
+          << ", " << feedback->pose.orientation.x
+          << ", " << feedback->pose.orientation.y
+          << ", " << feedback->pose.orientation.z
+          << "\nframe: " << feedback->header.frame_id
+          << " time: " << feedback->header.stamp.sec << "sec, "
+          << feedback->header.stamp.nsec << " nsec" );
+      break;
+
+    case visualization_msgs::InteractiveMarkerFeedback::MOUSE_DOWN:
+      ROS_INFO_STREAM( s.str() << ": mouse down" << mouse_point_ss.str() << "." );
+      break;
+
+    case visualization_msgs::InteractiveMarkerFeedback::MOUSE_UP:
+      ROS_INFO_STREAM( s.str() << ": mouse up" << mouse_point_ss.str() << "." );
+      break;
+  }
+
+  server->applyChanges();
+}
+
+
+void alignMarker( const InteractiveMarkerFeedbackConstPtr &feedback )
+{
+	geometry_msgs::Pose pose = feedback->pose;
+	pose.position.x = round(pose.position.x-0.5)+0.5;
+	pose.position.y = round(pose.position.y-0.5)+0.5;
+	ROS_INFO_STREAM( feedback->marker_name << ":"
+	<< " aligning position = "
+	<< feedback->pose.position.x
+	<< ", " << feedback->pose.position.y
+	<< ", " << feedback->pose.position.z
+	<< " to "
+	<< pose.position.x
+	<< ", " << pose.position.y
+	<< ", " << pose.position.z );
+
+	server->setPose( feedback->marker_name, pose );
+	server->applyChanges();
+}
+
+
+// Start interactive marker server 
+int main(int argc, char** argv)
+{
+
+
+
+}
